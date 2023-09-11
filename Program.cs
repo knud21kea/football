35431cdfdf7c23cd9e2bf39938bc4c8141fe55f1
knud21kea <grah0070@stud.kea.dk>
2023-09-12 00:29:30 +0200

    round result generation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using System.IO;
 
 namespace football;
 class Program
@@ -6,6 +7,11 @@ class Program
     static void Main(string[] args)
     {
         string[] teams = { "AGF", "BIF", "FCK", "HIF", "LBK", "FCM", "FCN", "OBK", "RFC", "SIF", "VBK", "VFF" };
-        RoundGenerator.GenerateRoundMatches(teams);
+        for (int r = 0; r < 22; r++)
+        {
+            string round = RoundGenerator.GenerateRoundMatches(teams, r);
+            File.WriteAllText("./CSV-files/SL/round-" + (r + 1) + ".csv", round);
+        }
+        Console.WriteLine(17 % 11);
     }
 }
--- RoundGenerator.cs
+++ RoundGenerator.cs
@@ -1,30 +1,35 @@
 static class RoundGenerator
 {
 
-    public static void GenerateRoundMatches(string[] teams)
+    public static string GenerateRoundMatches(string[] teams, int round)
     {
-        Console.WriteLine("Round 1");
+        string csvForRound = "";
         for (int m = 0; m < 6; m++)
         {
             string homeTeam = teams[m];
-            string awayTeam = teams[m + 6];
-            string[] twoTeams = {homeTeam, awayTeam};
-            Console.WriteLine(GenerateRoundData(twoTeams)); 
+            string awayTeam = teams[(m + 6 + round) % 11];
+            string[] twoTeams = { homeTeam, awayTeam };
+            if (round % 2 == 1)
+            {
+                Array.Reverse(twoTeams);
+            }
+            csvForRound += GenerateRoundData(twoTeams);
         }
+        return csvForRound;
     }
 
     public static string GenerateRoundData(string[] twoTeams)
     {
         // int round = 1;
-        string match = twoTeams[0] + ";" + twoTeams[1] + ";" + GetGoals() + "-" + GetGoals() + ";Other data;";
+        string match = twoTeams[0] + ";" + twoTeams[1] + ";" + GetGoals(0) + "-" + GetGoals(-0.1) + ";Other data;" + "\n";
         return match;
     }
 
-    public static int GetGoals()
+    public static int GetGoals(double bias) // cant score more than 3 goals away
     {
         Random random = new();
-        double rand = random.NextDouble();
-        if (rand < 0.3)
+        double rand = random.NextDouble() + bias; // goals are random weighted to lower values
+        if (rand < 0.35)
         {
             return 0;
         }
