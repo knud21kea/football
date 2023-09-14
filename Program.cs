e7471c6dd8bf151b9a0ba09e9222cf2a94e8af97
knud21kea <grah0070@stud.kea.dk>
2023-09-14 02:15:35 +0200

    22 generation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.X509Certificates;
 using System.IO;
 
 namespace football;
@@ -7,11 +7,28 @@ class Program
     static void Main(string[] args)
     {
         string teams = FileHandler.GetTeamAbbreviations("./CSV-files/SL/teams.csv");
-        string[] abbreviations = teams.Split(';');        
+        string[] abbreviations = teams.Split(';');
         for (int r = 0; r < 22; r++)
-        {           
-            string round = RoundGenerator.GenerateRoundMatches(abbreviations, r);
+        {
+            string round = RoundGenerator.GenerateRoundMatches22(abbreviations, r);
             File.WriteAllText("./CSV-files/SL/rounds/round-" + (r + 1) + ".csv", round);
         }
+        // after 22 rounds we need to generate 10 more rounds
+        // here the top 6 teams play each other home and away
+        // start with an ordered list and use a modified
+        // to test we assume that the first 6 remain in the top half
+        string[] topAbbreviations = new string[6];
+        string[] bottomAbbreviations = new string[6];
+        Array.Copy(abbreviations, 0, topAbbreviations, 0, 6);
+        Array.Copy(abbreviations, 6, bottomAbbreviations, 0, 6);
+        
+        /* for (int r = 0; r < 10; r++)
+        {
+            string upperRound = RoundGenerator.GenerateRoundMatches32(topAbbreviations, r);
+            string lowerRound = RoundGenerator.GenerateRoundMatches32(bottomAbbreviations, r);
+            string round = upperRound + lowerRound;
+            File.WriteAllText("./CSV-files/SL/rounds/round-" + (r + 23) + ".csv", round);
+        } */
     }
+
 }
--- RoundGenerator.cs
+++ RoundGenerator.cs
@@ -1,13 +1,45 @@
 static class RoundGenerator
 {
+    
+    static readonly string scheduleCode = "ABCDEFGHIJKL";
+    static readonly string[,] fixtures22 = {
+    {"DF","CE","AK","HI","BJ","GL"},
+    {"EL","CK","FJ","DI","BH","AG"},
+    {"JK","CI","AD","EF","BG","HL"},
+    {"CH","FK","GI","BE","AJ","DL"},
+    {"CD","GJ","EI","BF","AH","KL"},
+    {"IL","CJ","AE","DH","FG","BK"},
+    {"HK","DJ","EG","BI","CL","AF"},
+    {"HJ","AB","DE","IK","CG","FL"},
+    {"DG","FH","AI","JL","BC","EK"},
+    {"EH","AL","BD","GK","IJ","CF"},
+    {"AC","FI","BL","DK","GH","EJ"}
+    };
 
-    public static string GenerateRoundMatches(string[] teams, int round)
+    public static string GenerateRoundMatches22(string[] teams, int round)
     {
         string csvForRound = "";
         for (int m = 0; m < 6; m++)
         {
-            string homeTeam = teams[m];
-            string awayTeam = teams[(m + 6 + round) % 11];
+            string codes = fixtures22[round%11,m];
+            string homeTeam = teams[scheduleCode.IndexOf(codes.Substring(0,1))];
+            string awayTeam = teams[scheduleCode.IndexOf(codes.Substring(1,1))];
+            string[] twoTeams = { homeTeam, awayTeam };
+            if (round > 10) {
+                Array.Reverse(twoTeams);
+            }
+            csvForRound += GenerateRoundData(twoTeams);
+        }
+        return csvForRound;
+    }
+
+    public static string GenerateRoundMatches32(string[] teams, int round)
+    {
+        string csvForRound = "";
+        for (int m = 0; m < 3; m++)
+        {        
+            string homeTeam = teams[scheduleCode.IndexOf(fixtures22[round,m].Substring(0,1))];
+            string awayTeam = teams[scheduleCode.IndexOf(fixtures22[round,m].Substring(1,2))];
             string[] twoTeams = { homeTeam, awayTeam };
             if (round % 2 == 1)
             {
@@ -53,5 +85,9 @@ static class RoundGenerator
         {
             return 5;
         }
+    }
+
+    public static int GetAbbrIndex(string index) {
+        return scheduleCode.IndexOf(index);
     }
 }
